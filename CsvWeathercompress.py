11faa1e3603efeeaa51cc901f15e167ba3a1421a
g0rillaz <34920322+g0rillaz@users.noreply.github.com>
2020-01-30 23:01:41 +0100

    Fügt ein Semikolon an die 2. letzte Spalte hinzu.

--- CsvWeathercompress.py
+++ CsvWeathercompress.py
@@ -1,20 +1,17 @@
 import glob
 import os
 import pandas as pd
-from colors import bcolors
+# from colors import bcolors
 
 
-path = r'Quellpath der CSV-files' # input-path
-all_files = glob.glob(os.path.join(path, "*.csv"))
-names = [os.path.basename(x).split('.')[-2] for x in all_files]
-
-print("Es stehen Daten zu:",len(names),"Stationen zur verfügung!" )
+pfad = r'Quellpath der CSV-files' # input-path
+alle_datein = glob.glob(os.path.join(pfad, "*.csv"))
+namen = [os.path.basename(x).split('.')[-2] for x in alle_datein] # for all paths take only the base name split it at "." take the name behind "." in all_
+print("Es stehen Daten zu:",len(namen),"Stationen zur verfügung!" )
 
 df = pd.DataFrame()
 
-
-# df = pd.DataFrame()
-for file_, stationId in zip(all_files, names):
+for file_, stationId in zip(alle_datein, namen):
     file_df = pd.read_csv(file_, index_col=0, header=None)
     file_df['stationId'] = stationId
     print("Csv-Daten-frame der Station-Id: {} wird importiert".format(stationId))
